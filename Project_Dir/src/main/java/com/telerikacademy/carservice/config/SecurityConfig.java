b0037831400fb346dd2dae5563d9dbbaecc9f0c7
Yavor Naydenov <nadyenov.yavor@gmail.com>
2019-04-14 14:39:12 +0300

    Changes made to SecurityConfig during working on bug

--- Project_Dir/src/main/java/com/telerikacademy/carservice/config/SecurityConfig.java
+++ Project_Dir/src/main/java/com/telerikacademy/carservice/config/SecurityConfig.java
@@ -3,16 +3,15 @@ package com.telerikacademy.carservice.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 import javax.sql.DataSource;
-import java.util.Properties;
 
 @Configuration
 @EnableWebSecurity
@@ -20,55 +19,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private DataSource securityDataSource;
 
     @Autowired
-    public SecurityConfig (DataSource dataSource) {
+    public SecurityConfig(DataSource dataSource) {
         securityDataSource = dataSource;
+    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager() {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
+        jdbcUserDetailsManager.setDataSource(securityDataSource);
+        return jdbcUserDetailsManager;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/").hasRole("USER")
-                .antMatchers("/admin").hasRole("ADMIN")
-                .antMatchers("/register").hasRole("ADMIN")
+                .antMatchers("/admin", "/register").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
                 .loginProcessingUrl("/authenticateUser")
                 .permitAll()
-                .and()
-                .logout().permitAll();
+        .and()
+        .exceptionHandling().accessDeniedPage("/access-denied");
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.jdbcAuthentication().dataSource(securityDataSource);
-        auth.inMemoryAuthentication()
-                .withUser(User.withUsername("admin").password("{noop}pass1").roles("USER", "ADMIN"))
-                .withUser(User.withUsername("gosho").password("{noop}pass2").roles("USER"))
-                .withUser(User.withUsername("misho").password("{noop}pass3").roles("USER"));
-    }
+        auth.jdbcAuthentication().dataSource(securityDataSource);
+//        auth.inMemoryAuthentication()
+//                .withUser(User.withUsername("admin").password("{noop}pass1").roles("USER", "ADMIN"))
+//                .withUser(User.withUsername("gosho").password("{noop}pass1").roles("USER"));
 
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-
-    @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-
-        mailSender.setUsername("my.gmail@gmail.com");
-        mailSender.setPassword("password");
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-
-        return mailSender;
     }
 }
