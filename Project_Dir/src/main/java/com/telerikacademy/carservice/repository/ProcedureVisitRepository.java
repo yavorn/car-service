31942fcceb79601f495ae07293d5a2829ceef5d7
Rossen_Dell_PC <[rossen.halachev@gmail.com]>
2019-04-15 06:18:40 +0300

    Added ProcedureVisit Entity

---
+++ Project_Dir/src/main/java/com/telerikacademy/carservice/repository/ProcedureVisitRepository.java
@@ -0,0 +1,8 @@
+package com.telerikacademy.carservice.repository;
+
+
+import com.telerikacademy.carservice.models.ProcedureVisit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProcedureVisitRepository extends JpaRepository<ProcedureVisit, Integer> {
+}
---
+++ Project_Dir/src/main/java/com/telerikacademy/carservice/service/ProcedureVisitServiceImpl.java
@@ -0,0 +1,20 @@
+package com.telerikacademy.carservice.service;
+
+
+import com.telerikacademy.carservice.models.ProcedureVisit;
+import com.telerikacademy.carservice.repository.ProcedureVisitRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ProcedureVisitServiceImpl implements ProcedureVisitService {
+    @Autowired
+    ProcedureVisitRepository procedureVisitRepository;
+
+    @Override
+    public List<ProcedureVisit> getAllProcedureVisits() {
+        return procedureVisitRepository.findAll();
+    }
+}
