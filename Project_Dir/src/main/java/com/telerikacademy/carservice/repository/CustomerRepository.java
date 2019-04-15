d7dbbf60ca14afb8032ee68b2eec01eabc67c22e
Yavor Naydenov <nadyenov.yavor@gmail.com>
2019-04-15 17:20:46 +0300

    Corrected PK type in CustomerRepository
    of PK in JpaRepository extention, interface CustomerRepository

--- Project_Dir/src/main/java/com/telerikacademy/carservice/repository/CustomerRepository.java
+++ Project_Dir/src/main/java/com/telerikacademy/carservice/repository/CustomerRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface CustomerRepository extends JpaRepository<Customer, String> {
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     List<Customer> findAll();
 
     Customer findCustomerByEmail(String email);
