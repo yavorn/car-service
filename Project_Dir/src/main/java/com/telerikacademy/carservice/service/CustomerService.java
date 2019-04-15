441805847bbaae2353e3d8a2cbd473ba0ed4ebc1
Yavor Naydenov <nadyenov.yavor@gmail.com>
2019-04-15 15:02:26 +0300

    Created CustomerService interface and CustomerServiceImpls class.

---
+++ Project_Dir/src/main/java/com/telerikacademy/carservice/service/CustomerService.java
@@ -0,0 +1,10 @@
+package com.telerikacademy.carservice.service;
+
+import com.telerikacademy.carservice.models.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer> getAllCustomers();
+
+}
---
+++ Project_Dir/src/main/java/com/telerikacademy/carservice/service/CustomerServiceImpl.java
@@ -0,0 +1,4 @@
+package com.telerikacademy.carservice.service;
+
+public class CustomerServiceImpl {
+}
