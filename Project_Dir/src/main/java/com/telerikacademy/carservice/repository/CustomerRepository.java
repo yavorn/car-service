99e7f7408e79793e78fe55cfdbfa49465c495092
Yavor Naydenov <nadyenov.yavor@gmail.com>
2019-04-15 16:24:53 +0300

    Added methods to CustomerRepository
    List<Customer> findAll();
    Customer findCustomerByEmail(String email);

--- Project_Dir/src/main/java/com/telerikacademy/carservice/repository/CustomerRepository.java
+++ Project_Dir/src/main/java/com/telerikacademy/carservice/repository/CustomerRepository.java
@@ -3,6 +3,10 @@ package com.telerikacademy.carservice.repository;
 import com.telerikacademy.carservice.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository<Customer, String> {
+import java.util.List;
 
+public interface CustomerRepository extends JpaRepository<Customer, String> {
+    List<Customer> findAll();
+
+    Customer findCustomerByEmail(String email);
 }
