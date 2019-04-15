9c6edc107e26bd71cf12dc7a3e92ab532931ab1b
Yavor Naydenov <nadyenov.yavor@gmail.com>
2019-04-15 16:17:39 +0300

    Added UsernameExistsException.

--- Project_Dir/src/main/java/com/telerikacademy/carservice/exceptions/EmailExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.telerikacademy.carservice.exceptions;
-
-public class EmailExistsException extends RuntimeException {
-    public EmailExistsException(String message) {
-        super(message);
-    }
-}
