14ee3fdbef31ed0c5332a9681e325311fb3790dd
patzo.bg <plamen.e.todorov@gmail.com>
2019-04-17 22:30:29 +0300

    Add delete Make and it's Models

--- Project_Dir/src/main/java/com/telerikacademy/carservice/repository/ModelsRepository.java
+++ Project_Dir/src/main/java/com/telerikacademy/carservice/repository/ModelsRepository.java
@@ -4,6 +4,8 @@ import com.telerikacademy.carservice.models.Models;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface ModelsRepository extends JpaRepository<Models, Long> {
@@ -11,6 +13,9 @@ public interface ModelsRepository extends JpaRepository<Models, Long> {
     Models findModelsByModelID(Long id);
     void deleteModelsByMake_MakeID(Long id);
     void deleteAllByMake_MakeID(Long id);
+    void deleteByMake_MakeID(Long id);
+
+    List<Models> findModelsByMake_MakeID(Long id);
 
 
 }
