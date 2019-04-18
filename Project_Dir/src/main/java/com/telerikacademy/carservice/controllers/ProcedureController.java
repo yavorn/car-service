439e3889f8da8335cdbc1a02562af63b437f92a0
Rossen_Dell_PC <[rossen.halachev@gmail.com]>
2019-04-18 04:43:51 +0300

    Added All Procedures View and Create a Procedure View

--- Project_Dir/src/main/java/com/telerikacademy/carservice/controllers/ProcedureController.java
+++ Project_Dir/src/main/java/com/telerikacademy/carservice/controllers/ProcedureController.java
@@ -1,10 +1,16 @@
 package com.telerikacademy.carservice.controllers;
 
+import com.telerikacademy.carservice.models.Procedure;
 import com.telerikacademy.carservice.service.ProcedureService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/procedures")
@@ -19,6 +25,23 @@ public class ProcedureController {
     public String listProcedures(Model model){
         model.addAttribute("procedures", procedureService.getAllProcedures());
         return "list-procedures";
+    }
+
+    @GetMapping("/add-procedure")
+    public String addProcedureForm(Model model) {
+
+        model.addAttribute("procedure", new Procedure());
+        return "add-procedure";
+    }
+
+    @PostMapping("/add-procedure")
+    public String addProcedure(@Valid @ModelAttribute Procedure procedure , BindingResult bindingErrors) {
+
+        if(bindingErrors.hasErrors()) {
+            return "add-procedure";
+        }
+        procedureService.addProcedure(procedure);
+        return "redirect:/procedures";
     }
 
 }
---
+++ Project_Dir/src/main/resources/static/js/procedures.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+    $('#procedures-table').DataTable();
+});
+
