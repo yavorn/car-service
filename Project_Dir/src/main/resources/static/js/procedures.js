e60a28232b9e31f5de7a9a5b0e924f6f4c29ae73
Yavor Naydenov <nadyenov.yavor@gmail.com>
2019-04-25 19:24:15 +0300

    Added .DataTable function for #admin-homapage-table and #user-homapage-table-homapage-table.

--- Project_Dir/src/main/resources/static/js/procedures.js
+++ Project_Dir/src/main/resources/static/js/procedures.js
@@ -2,3 +2,18 @@ $(document).ready(function () {
     $('#procedures-table').DataTable();
 });
 
+$(document).ready(function () {
+    $('#admin-homapage-table').DataTable();
+});
+
+$('#admin-homapage-table').DataTable( {
+    buttons: ['pdf']
+} );
+
+$(document).ready(function () {
+    $('#user-homapage-table-homapage-table').DataTable();
+});
+
+$('#user-homapage-table').DataTable( {
+    buttons: ['pdf']
+} );
