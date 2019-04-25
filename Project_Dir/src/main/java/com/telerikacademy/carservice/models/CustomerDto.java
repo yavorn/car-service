60e998d635bf41dab3801bdbb1c9f56455329012
Yavor Naydenov <nadyenov.yavor@gmail.com>
2019-04-25 12:30:06 +0300

    Added field passwordConfirmation with getter/setter.

--- Project_Dir/src/main/java/com/telerikacademy/carservice/models/CustomerDto.java
+++ Project_Dir/src/main/java/com/telerikacademy/carservice/models/CustomerDto.java
@@ -13,7 +13,7 @@ import javax.validation.constraints.Size;
 
         private String password;
 
-        private String confirmedPassword;
+        private String passwordConfirmation;
 
         private String name;
 
@@ -54,11 +54,11 @@ import javax.validation.constraints.Size;
             this.password = password;
         }
 
-        public String getConfirmedPassword() {
-            return confirmedPassword;
+        public String getPasswordConfirmation() {
+            return passwordConfirmation;
         }
 
-        public void setConfirmedPassword(String confirmedPassword) {
-            this.confirmedPassword = confirmedPassword;
+        public void setPasswordConfirmation(String passwordConfirmation) {
+            this.passwordConfirmation = passwordConfirmation;
         }
     }
