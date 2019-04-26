5278c6594a4dbce6992516c9271a77578e8811e3
Yavor Naydenov <nadyenov.yavor@gmail.com>
2019-04-26 07:55:30 +0300

    Added a test.html page with ots test.js file to test the DataTables as homepage.

--- Project_Dir/src/main/resources/static/js/procedures.js
+++ Project_Dir/src/main/resources/static/js/procedures.js
@@ -1,19 +1,3 @@
 $(document).ready(function () {
     $('#procedures-table').DataTable();
 });
-
-$(document).ready(function () {
-    $('#admin-homapage-table').DataTable();
-});
-
-$('#admin-homapage-table').DataTable( {
-    buttons: ['pdf']
-} );
-
-$(document).ready(function () {
-    $('#user-homapage-table-homapage-table').DataTable();
-});
-
-$('#user-homapage-table').DataTable( {
-    buttons: ['pdf']
-} );
