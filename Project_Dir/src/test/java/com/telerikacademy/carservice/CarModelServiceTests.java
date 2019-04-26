6054d60fea464d484221becca72c788228828a70
patzo.bg <plamen.e.todorov@gmail.com>
2019-04-26 12:19:41 +0300

    Add new carService unit tests

--- Project_Dir/src/test/java/com/telerikacademy/carservice/CarModelServiceTests.java
+++ Project_Dir/src/test/java/com/telerikacademy/carservice/CarModelServiceTests.java
@@ -1,5 +1,6 @@
 package com.telerikacademy.carservice;
 
+import com.telerikacademy.carservice.exceptions.DatabaseItemNotFoundException;
 import com.telerikacademy.carservice.models.Make;
 import com.telerikacademy.carservice.models.Models;
 import com.telerikacademy.carservice.repository.MakeRepository;
@@ -55,7 +56,7 @@ public class CarModelServiceTests {
         carMakeAudi.setMakeID((long) 1);
         carMakeSeat.setMakeID((long) 2);
         carModelA6.setModelID((long) 1);
-        carModelRS6.setModelID((long)2);
+        carModelRS6.setModelID((long) 2);
         carModelLeon.setModelID((long) 3);
 
         carMakes.add(carMakeAudi);
@@ -73,14 +74,24 @@ public class CarModelServiceTests {
         // Arrange
         when(mockMakeRepository.findAllByOrderByMakeNameAsc())
                 .thenReturn(carMakes);
-
         // Act
         List<Make> result = carService.getAllMakes();
-
         // Assert
         assertEquals(2, result.size());
         assertEquals("Audi", result.get(0).getMakeName());
         assertEquals("Seat", result.get(1).getMakeName());
+        verify(mockMakeRepository, times(1)).findAllByOrderByMakeNameAsc();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void getAllMakes_shouldThrowNullPointerException() {
+        // Arrange
+        when(mockMakeRepository.findAllByOrderByMakeNameAsc())
+                .thenReturn(null);
+        // Act
+        List<Make> result = carService.getAllMakes();
+//        // Assert
+        assertEquals(0, result.size());
         verify(mockMakeRepository, times(1)).findAllByOrderByMakeNameAsc();
     }
 
@@ -101,48 +112,85 @@ public class CarModelServiceTests {
         verify(mockModelsRepository, times(1)).findAll();
     }
 
+    @Test(expected = NullPointerException.class)
+    public void getAllModels_shouldThrowNullPointerException() {
+        // Arrange
+        when(mockModelsRepository.findAll())
+                .thenReturn(null);
+        // Act
+        List<Models> result = carService.getAllModels();
+        // Assert
+        assertEquals(0, result.size());
+        verify(mockModelsRepository, times(1)).findAll();
+    }
+
+    @Test(expected = DatabaseItemNotFoundException.class)
+    public void getMakeById_shouldThrowDatabaseItemNotFoundException_whenInvalidIdIsPassed() {
+        //Arrange
+        Mockito.when(mockMakeRepository.findMakeByMakeID((long) 100000)).thenReturn(null);
+        // Act
+        Make carMake = carService.getMakeById((long) 100000);
+        // Assert
+        assertEquals(null, carMake);
+        verify(mockMakeRepository, times(2)).findMakeByMakeID((long) 1);
+    }
+
     @Test
     public void getMakeById_shouldReturnMakeWithIdOne_whenArgumentIsSetToOne() {
         //Arrange
-       Mockito.when(mockMakeRepository.findMakeByMakeID((long)1)).thenReturn(carMakeAudi);
+        Mockito.when(mockMakeRepository.findMakeByMakeID((long) 1)).thenReturn(carMakeAudi);
         // Act
-        Make carMake = carService.getMakeById((long)1);
+        Make carMake = carService.getMakeById((long) 1);
         // Assert
         assertEquals(carMakes.get(0), carMake);
         assertEquals("Audi", carMake.getMakeName());
-        verify(mockMakeRepository, times(2)).findMakeByMakeID((long)1);
+        verify(mockMakeRepository, times(2)).findMakeByMakeID((long) 1);
     }
+
+
 
 
     @Test
     public void getModelById_shouldReturnModelWithIdOne_whenArgumentIsSetToOne() {
         //Arrange
-        Mockito.when(mockModelsRepository.findModelsByModelID((long)1)).thenReturn(carModelA6);
+        Mockito.when(mockModelsRepository.findModelsByModelID((long) 1)).thenReturn(carModelA6);
 
         // Act
-        Models carModel = carService.getModelById((long)1);
+        Models carModel = carService.getModelById((long) 1);
 
         // Assert
         assertEquals(carModels.get(0), carModelA6);
         assertEquals("A6", carModel.getModelName());
-        verify(mockModelsRepository, times(2)).findModelsByModelID((long)1);
+        verify(mockModelsRepository, times(2)).findModelsByModelID((long) 1);
+    }
+
+    @Test(expected = DatabaseItemNotFoundException.class)
+    public void getModelById_shouldThrowDatabaseItemNotFoundException_whenInvalidIdIsPassed() {
+        Mockito.when(mockModelsRepository.findModelsByModelID((long) 100000)).thenReturn(null);
+
+        // Act
+        Models carModel = carService.getModelById((long) 100000);
+
+        // Assert
+        assertEquals(null, carModel);
+        verify(mockModelsRepository, times(2)).findModelsByModelID((long) 1);
     }
 
     @Test
     public void findModelsByMakeId_shouldReturnModelWithMakeIdOne_whenArgumentIsSetToOne() {
 
         // Arrange
-        when(mockModelsRepository.findModelsByMake_MakeID((long)1))
+        when(mockModelsRepository.findModelsByMake_MakeID((long) 1))
                 .thenReturn(carModels);
 
         // Act
-        List<Models> result = carService.findModelsByMakeID((long)1);
+        List<Models> result = carService.findModelsByMakeID((long) 1);
 
         // Assert
         assertEquals(2, result.size());
         assertEquals("A6", result.get(0).getModelName());
         assertEquals("RS6", result.get(1).getModelName());
-        verify(mockModelsRepository, times(1)).findModelsByMake_MakeID((long)1);
+        verify(mockModelsRepository, times(1)).findModelsByMake_MakeID((long) 1);
     }
 
 
