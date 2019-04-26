5008fc5c236e8164a90d94aaee00bd2e92f7586f
Yavor Naydenov <nadyenov.yavor@gmail.com>
2019-04-26 14:10:31 +0300

    Added void sendSimpleMessage(String to, String subject, String text) to EmailService interface;

--- Project_Dir/src/main/java/com/telerikacademy/carservice/service/contracts/EmailService.java
+++ Project_Dir/src/main/java/com/telerikacademy/carservice/service/contracts/EmailService.java
@@ -3,6 +3,8 @@ package com.telerikacademy.carservice.service.contracts;
 import org.springframework.mail.SimpleMailMessage;
 
 public interface EmailService {
+    void sendSimpleMessage(String to, String subject, String text);
+
     void sendSimpleMessageForPasswordResetUsingTemplate(String to, String subject, String text);
 
     void sendSimpleMessageUsingTemplateWhenCreatingCustomer(String to, SimpleMailMessage template, String... templateArgs);
