d825f00f863408e25b98d36b6039480755470a42
Yavor Naydenov <nadyenov.yavor@gmail.com>
2019-04-26 22:40:42 +0300

    Fixed access modifier of UserRightsNotDisabledException

--- Project_Dir/src/main/java/com/telerikacademy/carservice/exceptions/UserRightsNotDisabledException.java
+++ Project_Dir/src/main/java/com/telerikacademy/carservice/exceptions/UserRightsNotDisabledException.java
@@ -1,7 +1,7 @@
 package com.telerikacademy.carservice.exceptions;
 
 public class UserRightsNotDisabledException extends RuntimeException {
-    private UserRightsNotDisabledException(String username) {
+    public UserRightsNotDisabledException(String username) {
         super(String.format("%s has active rights!", username));
     }
 }
