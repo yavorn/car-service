06754bc3fef94c5ea377e573ca4bfd284fd92a3c
Yavor Naydenov <nadyenov.yavor@gmail.com>
2019-04-30 22:57:08 +0300

    Set CustomerCarsRepository customerCarsRepository field to private;

--- Project_Dir/src/main/java/com/telerikacademy/carservice/service/CustomerCarsServiceImpl.java
+++ Project_Dir/src/main/java/com/telerikacademy/carservice/service/CustomerCarsServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Service
 public class CustomerCarsServiceImpl implements CustomerCarsService {
 
-    CustomerCarsRepository customerCarsRepository;
+    private CustomerCarsRepository customerCarsRepository;
 
     @Autowired
     public CustomerCarsServiceImpl(CustomerCarsRepository customerCarsRepository){
