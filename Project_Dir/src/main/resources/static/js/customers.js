5daf93ea3322ecc0282cffb64709e77a201640d3
Rossen_Lenovo_PC <rossen.halachev@gmail.com>
2019-05-01 18:31:05 +0300

    Fixed row numbering in tables

--- Project_Dir/src/main/resources/static/js/customers.js
+++ Project_Dir/src/main/resources/static/js/customers.js
@@ -2,4 +2,18 @@
 let apiUrl = 'http://localhost:8080/';
 
 //Initialize Customers table
-$('#customers-table').DataTable();
+let table = $('#customers-table').DataTable( {
+    "columnDefs": [ {
+        "searchable": false,
+        "orderable": false,
+        "targets": 0
+    } ],
+    "order": [[ 1, 'asc' ]]
+} );
+
+table.on( 'order.dt search.dt', function () {
+    table.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+        cell.innerHTML = i+1;
+    } );
+} ).draw();
+
--- Project_Dir/src/main/resources/static/js/procedures.js
+++ Project_Dir/src/main/resources/static/js/procedures.js
@@ -2,7 +2,28 @@
 let apiUrl = 'http://localhost:8080/';
 
 //Initilize Procedure table
-$('#procedures-table').DataTable();
+let table = $('#procedures-table').DataTable( {
+    "columnDefs": [ {
+        "searchable": false,
+        "orderable": false,
+        "targets": 0
+    } ],
+    "order": [[ 1, 'asc' ]]
+} );
+
+table.on( 'order.dt search.dt', function () {
+    table.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+        cell.innerHTML = i+1;
+    } );
+} ).draw();
+
+
+
+
+
+
+
+
 
 function addProcedure() {
 
