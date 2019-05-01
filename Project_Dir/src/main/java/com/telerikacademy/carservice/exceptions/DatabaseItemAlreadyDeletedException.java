fd259488f59af1425ecee2fea17569c702e7d215
Yavor Naydenov <nadyenov.yavor@gmail.com>
2019-05-01 14:11:30 +0300

    Refactor of DatabaseItemAlreadyDeletedException and UserRightsNotDisabledException to work with String message for better readability.

--- Project_Dir/src/main/java/com/telerikacademy/carservice/exceptions/DatabaseItemAlreadyDeletedException.java
+++ Project_Dir/src/main/java/com/telerikacademy/carservice/exceptions/DatabaseItemAlreadyDeletedException.java
@@ -1,13 +1,7 @@
 package com.telerikacademy.carservice.exceptions;
 
 public class DatabaseItemAlreadyDeletedException extends RuntimeException {
-    public DatabaseItemAlreadyDeletedException(String item) {
-        super(String.format("%s is already deleted", item));
+    public DatabaseItemAlreadyDeletedException(String message) {
+        super(message);
     }
-
-    public DatabaseItemAlreadyDeletedException(String item, Long id) {
-        super(String.format("%s with ID: %d already deleted.", item, id));
-    }
-
-
 }
--- Project_Dir/src/main/java/com/telerikacademy/carservice/exceptions/UserRightsNotDisabledException.java
+++ Project_Dir/src/main/java/com/telerikacademy/carservice/exceptions/UserRightsNotDisabledException.java
@@ -1,7 +1,7 @@
 package com.telerikacademy.carservice.exceptions;
 
 public class UserRightsNotDisabledException extends RuntimeException {
-    public UserRightsNotDisabledException(String username) {
-        super(String.format("%s has active rights!", username));
+    public UserRightsNotDisabledException(String message) {
+        super(message);
     }
 }
