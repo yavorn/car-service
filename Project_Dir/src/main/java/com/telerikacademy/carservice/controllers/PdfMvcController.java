ea0b52b04b3dbd50ef7ab08b9313b8d4eaf5a012
Yavor Naydenov <nadyenov.yavor@gmail.com>
2019-05-04 18:42:33 +0300

    Crated PdfMvcController class.

--- Project_Dir/src/main/java/com/telerikacademy/carservice/controllers/PdfMvcController.java
+++ Project_Dir/src/main/java/com/telerikacademy/carservice/controllers/PdfMvcController.java
@@ -1,10 +1,14 @@
 package com.telerikacademy.carservice.controllers;
 
+import com.telerikacademy.carservice.exceptions.DatabaseItemNotFoundException;
 import com.telerikacademy.carservice.models.CarEvent;
+import com.telerikacademy.carservice.models.Procedure;
 import com.telerikacademy.carservice.service.contracts.CarEventService;
 import com.telerikacademy.carservice.service.contracts.PdfService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,34 +16,41 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
+import java.util.Set;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping
 public class PdfMvcController {
-    private CarEventService eventService;
+    private static final String NO_EVENT_FOUND_ERROR_MESSAGE = "Car event with id %d not found";
+    private static final String NO_PROCEDURES_FOUND_FOR_CUSTOMER_CAR = "There are no car events for car with id %d";
+    private CarEventService carEventService;
     private ServletContext context;
     private PdfService pdfService;
 
     @Autowired
-    public PdfMvcController(CarEventService eventService,
-                            ServletContext context, PdfService pdfService) {
-        this.eventService = eventService;
+    public PdfMvcController(CarEventService carEventService,
+                            ServletContext context,
+                            PdfService pdfService) {
+        this.carEventService = carEventService;
         this.context = context;
         this.pdfService = pdfService;
     }
 
-    @RequestMapping(value = {"/car-visit/pdf/{id}", "/admin/car-visit/pdf/{id}"}, method = RequestMethod.GET)
-    public void createPdf(@PathVariable long id, HttpServletRequest request, HttpServletResponse response) {
-        CarEvent carEvent = eventService.getCarEventByID(id);
+    @RequestMapping(value = "/createPdf/{carEventId}", method = RequestMethod.GET)
+    public void createPdf(@PathVariable long carEventId, HttpServletRequest request, HttpServletResponse response) {
 
-        boolean isPdfGen = pdfService.createPdf(carEvent,  context, request, response);
+        CarEvent carEvent = carEventService.getCarEventByID(carEventId);
+        if (carEvent == null) throw new DatabaseItemNotFoundException(String.format(NO_EVENT_FOUND_ERROR_MESSAGE, carEventId));
+        Set<Procedure> procedures = carEvent.getProcedures();
+        if (procedures.size() == 0) throw new DatabaseItemNotFoundException(String.format(NO_PROCEDURES_FOUND_FOR_CUSTOMER_CAR,
+                carEvent.getCustomerCar().getCustomerCarID()));
 
-        if (isPdfGen) {
-            String fullPath = request.getServletContext().getRealPath("/resources/reports/"
-                    + carEvent.getDate() + ", " + carEvent.getCustomerCar().getLicensePlate() +  ".pdf");
+        boolean isPdfCreated = pdfService.createPdf(carEvent, context, request, response);
+
+        if (isPdfCreated) {
+            String fullPath = request.getServletContext().getRealPath("/resources/reports/" + "reference" +  ".pdf");
             pdfService.fileDownload(fullPath, response);
-           // procedureVisitService.editPdfGenerated(procedureVisitService.findProcedureVisitByCarEventId(carEvent.getCarEventID()));
+            carEventService.editPdfGenerated(carEvent);
         }
     }
 }
