1d1d747d55c8b832d3d49cba80b915e456f255e1
Yavor Naydenov <nadyenov.yavor@gmail.com>
2019-05-05 09:59:52 +0300

    Replaced List<Customer> findAll() with List<Customer> findAllByIsDeletedFalse().

--- Project_Dir/src/main/java/com/telerikacademy/carservice/repository/CustomerRepository.java
+++ Project_Dir/src/main/java/com/telerikacademy/carservice/repository/CustomerRepository.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    List<Customer> findAll();
+    List<Customer> findAllByIsDeletedFalse();
 
     Customer findCustomerByEmail(String email);
 
