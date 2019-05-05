d9763ae3d92057e8550bcfacfa486011b6157ece
Yavor Naydenov <nadyenov.yavor@gmail.com>
2019-05-05 11:32:57 +0300

    Implemented role-based redirection after login.

--- Project_Dir/src/main/java/com/telerikacademy/carservice/config/SecurityConfig.java
+++ Project_Dir/src/main/java/com/telerikacademy/carservice/config/SecurityConfig.java
@@ -25,7 +25,7 @@ import javax.sql.DataSource;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private DataSource securityDataSource;
-    private final String[] ADMIN_RIGHTS = {"/", "/admin-portal", "/customer", "/customers", "/procedures", "/cars", "/admin", "/cars/edit-make/*", "/new-customer-car"};
+    private final String[] ADMIN_RIGHTS = {"/admin-portal", "/customer", "/customers", "/procedures", "/cars", "/admin", "/cars/edit-make/*", "/new-customer-car"};
     private final String[] USER_RIGHTS = {"/", "/carevents/pdf/*"};
 
     @Autowired
