962b33c5d5f22f9a02d9999955b291551d6097a3
Yavor Naydenov <nadyenov.yavor@gmail.com>
2019-05-05 11:26:34 +0300

    Implemented role-based redirection after login.

--- Project_Dir/src/main/java/com/telerikacademy/carservice/config/SecurityConfig.java
+++ Project_Dir/src/main/java/com/telerikacademy/carservice/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.telerikacademy.carservice.config;
 
+import com.telerikacademy.carservice.controllers.handlers.MySimpleUrlAuthenticationSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
@@ -14,6 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.sql.DataSource;
@@ -22,7 +25,8 @@ import javax.sql.DataSource;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private DataSource securityDataSource;
-    private final String[] ADMIN_RIGHTS = {"/admin-portal", "/customer", "/admin", "/cars/edit-make/*", "/new-customer-car"};
+    private final String[] ADMIN_RIGHTS = {"/", "/admin-portal", "/customer", "/customers", "/procedures", "/cars", "/admin", "/cars/edit-make/*", "/new-customer-car"};
+    private final String[] USER_RIGHTS = {"/", "/carevents/pdf/*"};
 
     @Autowired
     public SecurityConfig(DataSource dataSource) {
@@ -36,6 +40,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return jdbcUserDetailsManager;
     }
 
+    @Bean("authenticationManager")
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler myAuthenticationSuccessHandler() {
+        return new MySimpleUrlAuthenticationSuccessHandler();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -47,30 +62,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.authorizeRequests()
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                    .antMatchers("/password", "/password-confirmation").permitAll()
-                    .antMatchers("/", "/carevents/pdf/*").hasRole("USER")
-                    .antMatchers(ADMIN_RIGHTS).hasRole("ADMIN")
-                    .anyRequest().authenticated()
+                .antMatchers("/password", "/password-confirmation").permitAll()
+                .antMatchers(USER_RIGHTS).hasRole("USER")
+                .antMatchers(ADMIN_RIGHTS).hasRole("ADMIN")
+                .anyRequest().authenticated()
                 .and()
-                    .formLogin()
-                    .loginPage("/login")
-                    .loginProcessingUrl("/authenticateUser")
-                    .defaultSuccessUrl("/", true)
-                    .permitAll()
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/authenticateUser")
+                .successHandler(myAuthenticationSuccessHandler())
+//                .defaultSuccessUrl("/", true)
+                .permitAll()
                 .and()
-                    .logout()
-                    .invalidateHttpSession(true)
-                    .clearAuthentication(true)
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                    .logoutSuccessUrl("/login?logout")
-                    .permitAll()
+                .logout()
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout")
+                .permitAll()
                 .and()
-                    .exceptionHandling()
-                    .accessDeniedPage("/access-denied")
+                .exceptionHandling()
+                .accessDeniedPage("/access-denied")
                 .and()
-                    .httpBasic()
+                .httpBasic()
                 .and()
-                    .csrf().disable();
+                .csrf().disable();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        // @formatter:off
+        auth.jdbcAuthentication().dataSource(securityDataSource);
+        // @formatter:on
     }
 
     @Override
---
+++ Project_Dir/src/main/java/com/telerikacademy/carservice/controllers/handlers/MySimpleUrlAuthenticationSuccessHandler.java
@@ -0,0 +1,80 @@
+package com.telerikacademy.carservice.controllers.handlers;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Collection;
+
+public class MySimpleUrlAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    private Log logger = LogFactory.getLog(this.getClass());
+    private static final String UNABLE_TO_REDIRECT_MESSAGE = "Response has already been committed. Unable to redirect to ";
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
+        handle(request, response, authentication);
+        clearAuthenticationAttributes(request);
+    }
+
+    private void handle(HttpServletRequest request,
+                        HttpServletResponse response,
+                        Authentication authentication) throws IOException {
+        String targetUrl = determineTargetUrl(authentication);
+
+        if (response.isCommitted()) {
+            logger.debug(UNABLE_TO_REDIRECT_MESSAGE + targetUrl);
+            return;
+        }
+        redirectStrategy.sendRedirect(request, response, targetUrl);
+    }
+
+    private String determineTargetUrl(Authentication authentication) {
+        boolean isUser = false;
+        boolean isAdmin = false;
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        for (GrantedAuthority grantedAuthority : authorities) {
+            if (grantedAuthority.getAuthority().equals("ROLE_USER")) {
+                isUser = true;
+                break;
+            } else if (grantedAuthority.getAuthority().equals("ROLE_ADMIN")) {
+                isAdmin = true;
+                break;
+            }
+        }
+
+        if (isAdmin) {
+            return "/admin-portal" ;
+        } else if (isUser) {
+            return "/procedures";
+        } else {
+            throw new IllegalStateException();
+        }
+    }
+
+    private void clearAuthenticationAttributes(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return;
+        }
+        session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+    }
+
+    public void setRedirectStrategy(RedirectStrategy redirectStrategy) {
+        this.redirectStrategy = redirectStrategy;
+    }
+    protected RedirectStrategy getRedirectStrategy() {
+        return redirectStrategy;
+    }
+}
